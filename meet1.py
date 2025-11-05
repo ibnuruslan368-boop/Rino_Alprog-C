9583eee92fc56bbedfb24165f215b38165f9e8e2
ibnuruslan368-boop <ibnuruslan368@gmail.com>
2025-11-05 15:59:47 +0800

    Add files via upload

---
+++ meet1.py
@@ -0,0 +1,11 @@
+print("NAMA:Rino ibnu ruslan madar ladoangin")
+print("NIM:25241081")
+print("KELAS:PTI C")
+print("nim:25241081")
+#memindahkan baris alt+atas atau bawah 
+#cara copy satu baris arahkan kursor lalu tahan alt+shift
+#cara copy satu baris arahkan kursor lalu tahan alt+shift
+#cara copy satu baris arahkan kursor lalu tahan alt+shift
+a=10
+b=2
+print(a+b)
