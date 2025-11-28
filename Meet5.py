ba78c955189e0052eeb6529dc45941c97b25e380
ibnuruslan368-boop <ibnuruslan368@gmail.com>
2025-11-28 21:15:39 +0800

    Add files via upload

---
+++ Meet5.py
@@ -0,0 +1,29 @@
+# operasi aritmatika (x : + -)
+
+# penjumlahan
+a = 17
+b = 3
+hasil = a + b
+print(a, "+", b, "=", hasil)
+
+# pengurangan -
+# perkalian *
+# pembagian /
+hasil = a / b
+print(a, "/", b, "=", hasil)
+
+# perpangkatan **
+
+# modulus (sisa bagi)
+hasil = a % b
+print(a, "modulus", b, "=", hasil)
+
+# floor divison // (pembulatan)
+hasil = a // b
+print(a, "floor div", b, "=", hasil)
+
+# operasi prioritas
+# 1. ()
+# 2. pangkat akar
+# 3. x :
+# 4. + -
